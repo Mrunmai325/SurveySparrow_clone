a552f100ba8194c4584a7d85c383295831314e88
Mrunmai325 <2023bit049@sggs.ac.in>
2025-08-11 23:33:14 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,1 +1,17 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from .models import Base
 
+# Database connection
+engine = create_engine("sqlite:///survey.db")
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
