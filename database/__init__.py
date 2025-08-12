be340f18905bbe219766c5ac9868b33e7c05f2e7
Mrunmai325 <2023bit049@sggs.ac.in>
2025-08-12 09:18:32 +0530

    Update __init__.py

--- database/__init__.py
+++ database/__init__.py
@@ -1,12 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from .models import Base
 
-engine = create_engine("sqlite:///survey.db")
+# For SQLite:
+engine = create_engine('sqlite:///survey.db')
+# For PostgreSQL:
+# engine = create_engine('postgresql://user:password@localhost/dbname')
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-def init_db():
-    Base.metadata.create_all(bind=engine)
 
 def get_db():
     db = SessionLocal()
